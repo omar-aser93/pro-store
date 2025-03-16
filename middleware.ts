fc2b2ebe75383cb36f4fd381160f7de851fd0203
omar-aser93 <omaraser93@yahoo.com>
2025-03-17 01:01:11 +0200

    text fix

--- middleware.ts
+++ middleware.ts
@@ -13,9 +13,10 @@ export default async function middleware(req: NextRequest) {
 
   // If user is not authenticated and tries to access protected paths, redirect to login & save current path as callbackUrl param
   if (!token && protectedPaths.some((pattern) => pattern.test(req.nextUrl.pathname))) {    
-    const signInUrl = new URL("/sign-in", req.url);                   //Create a sign-in URL
-    signInUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);  //Add (current path as callbackUrl) to the sign-in URL
-    return NextResponse.redirect(signInUrl);                          //Redirect to the updated sign-in URL
+    // const signInUrl = new URL("/sign-in", req.url);                   //Create a sign-in URL
+    // signInUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);  //Add (current path as callbackUrl) to the sign-in URL
+    // return NextResponse.redirect(signInUrl);                          //Redirect to the updated sign-in URL
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
   // If user is not an admin and tries to access an admin route, redirect to home
@@ -28,8 +29,9 @@ export default async function middleware(req: NextRequest) {
   const publicPaths = [/^\/sign-in/, /^\/sign-up/];
   // If user is authenticated and tries to access public paths (e.g., sign-in, sign-up), redirect to home or callbackUrl
   if (token && publicPaths.some((pattern) => pattern.test(req.nextUrl.pathname))) {
-    const callbackUrl = req.nextUrl.searchParams.get("callbackUrl") || "/";
-    return NextResponse.redirect(new URL(callbackUrl, req.url));
+    // const callbackUrl = req.nextUrl.searchParams.get("callbackUrl") || "/";
+    // return NextResponse.redirect(new URL(callbackUrl, req.url));
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();           // middleware proceeds when no redirect is triggered
