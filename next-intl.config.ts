f1444fe3797121c71da17a28606510e63983b4bd
omar-mohamed <omaraser93@yahoo.com>
2026-01-16 20:19:34 +0200

    local fix

---
+++ next-intl.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next-intl').NextIntlConfig} */
+module.exports = {
+  locales: ['en', 'ar'],           // Supported locales
+  defaultLocale: 'en',             // Default language
+  messagesDirectory: './messages', // Path to your translation JSON files
+}
